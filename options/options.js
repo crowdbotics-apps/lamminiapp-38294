b64ae96ba4a6189c1f9e68c123c14a6598e743b7
Crowdbotics <team@crowdbotics.com>
2023-01-13 20:30:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lamminiapp-38294",
+  url: "https://lamminiapp-38294.botics.co",
+  api: "https://lamminiapp-38294.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
